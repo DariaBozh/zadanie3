58894e702879c78592f54c1008264019f8ffb943
Daria Bozh <darling.bozh12326@gmail.com>
2025-06-10 01:56:17 +0300

    Destroy and delete functions

--- zadanie11-5-3.c
+++ zadanie11-5-3.c
@@ -2,6 +2,10 @@
 #include <stdlib.h>
 #include <time.h>
 
+#define CTWL_OK 0
+#define CTWL_FAIL 1
+ 
+
 // вузол
 typedef struct TWN{
   float data;
@@ -9,10 +13,12 @@ typedef struct TWN{
   struct TWN *next;
  }TWN;
  
+ 
 // Це керівна структура — вона не зберігає елементи напряму, а лише вказує на cur
 typedef struct{
  TWN *cur;
  }CTWL;
+ 
  
 // створює новий
 CTWL *ctwl_create_empty(void){ 
@@ -21,9 +27,8 @@ CTWL *ctwl_create_empty(void){
 	return list;
 }
 
-// інсерт деліт
-TWN *ctwl_insert_left(CTWL* list, float val);
 
+// інсерт та деліт
 TWN *ctwl_insert_right(CTWL* list, float val){
 	TWN *new_node = (TWN*)malloc(sizeof(TWN));
 	new_node->data = val;
@@ -43,7 +48,32 @@ TWN *ctwl_insert_right(CTWL* list, float val){
 	return new_node;
 }
 
-char ctwl_delete(CTWL* list);
+TWN *ctwl_insert_left(CTWL* list, float val); //nie je potrebne realizovat, ale nabuduce mozno
+
+char ctwl_delete(CTWL* list){
+	if (list->cur == NULL)
+		return CTWL_OK;
+	
+	TWN *to_delete = list->cur;
+	
+	if (to_delete->next == to_delete){
+		free(to_delete);
+		list->cur = NULL;
+		return CTWL_FAIL;
+	}
+	
+	TWN *prev = to_delete->prev;
+    TWN *next = to_delete->next;
+    
+    prev->next = next;
+    next->prev = prev;
+    
+    list->cur = next;
+    free(to_delete);
+    
+	return CTWL_OK;
+}
+
 
 // рандом потрібної довжини
 CTWL *ctwl_create_random(unsigned int size){
@@ -56,10 +86,28 @@ CTWL *ctwl_create_random(unsigned int size){
 	return list;
 }
 
-// руйнує
-void ctwl_destroy(CTWL* list);
 
-// vypis
+// руйнує
+void ctwl_destroy(CTWL* list){
+	if(list == NULL || list->cur == NULL)
+	free(list);
+	return;
+	
+	TWN *start = list->cur;
+	TWN *node = start->next;
+	
+	while(node != start){
+		TWN *tmp = node->next;
+		free(node);
+		node = tmp;
+	}
+	
+	free(start);
+	free(list);
+}
+
+
+// виписує
 void ctwl_print(CTWL *list){
 	if(list->cur == NULL){
 		printf("Zoznam je prazny\n");
@@ -72,15 +120,22 @@ void ctwl_print(CTWL *list){
 		printf("%.2f ", node->data);
 		node = node->next;	
 	} while (node != start);
-	printf("\n");
+	printf("\n\n");
 }
 
+
 // зміна курсора
-void ctwl_cur_step_right(CTWL *list);
-void ctwl_cur_step_left(CTWL *list);
+void ctwl_cur_step_right(CTWL *list){
+	if(list->cur != NULL)
+	list->cur = list->cur->next;
+}
+
+void ctwl_cur_step_left(CTWL *list); //nie je potrebne realizovat, ale nabuduce mozno
+
 
 // головна функція
 void ctwl_concatenate(CTWL *a, CTWL *b);
+
 
 int main(){
 	srand(time(NULL));
